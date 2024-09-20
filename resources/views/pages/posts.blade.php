b99bb31c97e126d8e05a595e69690ab6c565578a
Iftala <circlebytes.iftala@gmail.com>
2024-09-20 13:32:56 +0700

    style: add styling to post and posts

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -1,17 +1,62 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    @foreach ($posts as $post)
+    {{-- @foreach ($posts as $post)
         <article class="max-w-screen-md py-8 border-b border-gray-500">
             <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900 hover:underline">{{ $post['title'] }}</h2>
             <div class="text-base text-gray-500">
                 <a href="">{{ $post['author'] }}</a> | {{ $post['date'] }}
             </div>
             <p class="my-4 font-light">
-                {{-- {{Str::limit($post['body'],100)}} --}}
                 {{ $post['body'] }}
             </p>
-            {{-- href="/posts/{{$id}}"  --}}
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500">Read more &raquo;</a>
         </article>
-    @endforeach
+    @endforeach --}}
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div class="grid gap-8">
+                @foreach ($posts as $post)
+                <article
+                    class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex justify-between items-center mb-5 text-gray-500">
+                        <span
+                            class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
+                                </path>
+                            </svg>
+                            Blog
+                        </span>
+                        <span class="text-sm">{{ $post['date'] }}</span>
+                    </div>
+                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
+                            href="#">{{ $post['title'] }}</a></h2>
+                    <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ $post['body'] }}</p>
+                    <div class="flex justify-between items-center">
+                        <div class="flex items-center space-x-4">
+                            <img class="w-7 h-7 rounded-full"
+                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                                alt="Jese Leos avatar" />
+                            <span class="font-medium dark:text-white">
+                                {{ $post['author'] }}
+                            </span>
+                        </div>
+                        <a href="/posts/{{ $post['slug'] }}"
+                            class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                            Read more
+                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </article>
+                @endforeach
+            </div>
+        </div>
+    </section>
 </x-layout>
